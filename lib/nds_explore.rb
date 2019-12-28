7be3e7ec07ca8a799c75af7596f850030cb71943
Lugh Wilson <lughjw@gmail.com>
2019-12-28 05:07:16 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -6,15 +6,13 @@ def pretty_print_nds(nds)
   # Change the code below to pretty print the nds with pp
   puts nds
   for i in 0...nds.length do
-    puts "nds[#{i}][0]"
+    puts "nds[#{i}]"
     puts nds[i][:name]
     puts nds[i][:movies]
     #puts nds[i][:studio]
     #puts nds[i][:worldwide_gross]
     #puts nds[i][:release_year]
-    puts nds[i][]
-    puts "nds[#{i}][1]"
-    puts nds[i][1]
+    
     #puts nds[i][:movies]
     
   end
