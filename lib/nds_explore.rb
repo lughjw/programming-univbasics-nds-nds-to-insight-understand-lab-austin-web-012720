c840de22ab743a8a69d54d3a9f51ab608bf8c048
Lugh Wilson <lughjw@gmail.com>
2019-12-28 05:41:54 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -31,7 +31,7 @@ def toDollars(num)
   start = 1000000000 #1 billion
   num_array = []
   
-  while start > 1
+  while start > 0
     if num >= start
       num_array[0] = num/start
       num -= num_array[0]
