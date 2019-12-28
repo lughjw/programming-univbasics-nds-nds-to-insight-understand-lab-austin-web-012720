b88b0635e0129c7e8789758c757cea783fa29413
Lugh Wilson <lughjw@gmail.com>
2019-12-28 04:48:23 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -9,7 +9,7 @@ def pretty_print_nds(nds)
     puts "outer col #{i} has #{nds[i].length} rows"
     
     for j in 0...nds[i].length
-      if nds[i] != nil
+      if nds[i][j] != nil
         puts "2nd col #{j} has #{nds[i][j].length} rows"
       end
     end
